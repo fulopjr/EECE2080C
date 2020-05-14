887033037c97dfa799f32745a9110f5709b38d68
fulopjr <65326412+fulopjr@users.noreply.github.com>
2020-05-14 13:30:26 -0400

    Add files via upload

---
+++ comparedelta.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double compareDelta(double num1, double num2, double delta)
+{
+    int Out;
+    if(fabs(num1 - num2) <= delta)
+    {
+        Out = 1;
+    }
+    else {
+        Out = 0;
+    }
+    return Out;
+}
+int main() {
+
+   /* Type your code here. */
+
+    double Number1 = 1.205;
+    double Number2 = 1.305;
+    double Delta = 0.100;
+
+    cout << "CompareDelta() Number1: " << Number1 << " Number2: " << Number2 << " Delta " << Delta << " returned: "
+         << compareDelta(Number1, Number2, Delta) << " should be 1 " << endl;
+    Number1 = 1;
+    Number2 = 2;
+    cout << "CompareDelta() Number1: " << Number1 << " Number2: " << Number2 << " Delta " << Delta << " returned: "
+         << compareDelta(Number1, Number2, Delta) << " should be 0 " << endl;
+    Number1 = 2;
+    Number2 = 4;
+    Delta = 2.5;
+    cout << "CompareDelta() Number1: " << Number1 << " Number2: " << Number2 << " Delta " << Delta << " returned: "
+         << compareDelta(Number1, Number2, Delta) << " should be 1 " << endl;
+    Number1 = 1.3058;
+    Number2 = 1.3059;
+    Delta   = 0.00001;
+    cout << "CompareDelta() Number1: " << Number1 << " Number2: " << Number2 << " Delta " << Delta << " returned: "
+         << compareDelta(Number1, Number2, Delta) << " should be 0 " << endl;
+
+   return 0;
+}
