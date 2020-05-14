07a5c11a2ec2d7e310eaecd1436f13dc861f2900
fulopjr <65326412+fulopjr@users.noreply.github.com>
2020-05-14 13:35:32 -0400

    Add files via upload

---
+++ quadrant.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+
+using namespace std;
+
+int whichQuadrant(float x, float y){
+int q;
+    if(x > 0 && y > 0)
+    {
+       q = 1;
+    }
+    else if (x < 0 && y > 0 )
+    {
+        q = 2;
+    }
+    else if (x < 0 && y < 0 )
+    {
+        q = 3;
+    }
+    else if (x > 0 && y < 0)
+    {
+        q = 4;
+    }
+    else if (x == 0.0 && y == 0.0)
+    {
+        q = 'C';
+    }
+    else if (x == 0.0 && y != 0.0)
+    {
+        q = 'Y';
+    }
+    else
+    {
+       q = 'X';
+    }
+    return q;
+}
+
+
+string message(int q) {
+    string msg;
+    if(q == 'C') msg = "On Center";
+    if(q == 'X') msg = "On XAxis";
+    if(q == 'Y') msg = "On YAxis";
+    if(q ==  1)  msg = "Q1";
+    if(q ==  2)  msg = "Q2";
+    if(q ==  3)  msg = "Q3";
+    if(q ==  4)  msg = "Q4";
+    return msg;
+}
+
+int main() { // No need to change main()
+    double  x = 0;
+    double y = 0;
+
+    cout << "For XY Pair: " << x << "," << y << " function returns: "
+         <<  message(whichQuadrant(x,y))
+         << " should be On Center" << endl;
+
+    x = 0;
+    y = 4.8;
+    cout << "For XY Pair: " << x << "," << y << " function returns: "
+         <<  message(whichQuadrant(x,y))
+         << " should be YAxis" << endl;
+
+    y = 0;
+    x = 4.8;
+    cout << "For XY Pair: " << x << "," << y << " function returns: "
+         <<  message(whichQuadrant(x,y))
+         << " should be XAxis" << endl;
+
+    x = 4.8;
+    y = 4.8;
+    cout << "For XY Pair: " << x << "," << y << " function returns: "
+         <<  message(whichQuadrant(x,y))
+         << " should be On Q1" << endl;
+
+    x = -7;
+    y = -4.8;
+    cout << "For XY Pair: " << x << "," << y << " function returns: "
+         <<  message(whichQuadrant(x,y))
+         << " should be On Q3" << endl;
+
+    x = 7;
+    y = -3;
+    cout << "For XY Pair: " << x << "," << y << " function returns: "
+         <<  message(whichQuadrant(x,y))
+         << " should be On Q4" << endl;
+
+    x = -9;
+    y = 8;
+    cout << "For XY Pair: " << x << "," << y << " function returns: "
+         <<  message(whichQuadrant(x,y))
+         << " should be On Q2" << endl;
+
+    return 0;
+}
